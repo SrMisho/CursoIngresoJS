4056ff0ebab7e5a9dcbed1c8d409b58b4d06900a
SrMisho <60608645+SrMisho@users.noreply.github.com>
2020-02-13 18:18:29 -0300

    Tarea
    Tarea

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,12 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+    var edad;
+    var situacion;
 
-
+    edad = parseInt(document.getElementById("edad").value);
+    situacion = document.getElementById("estadoCivil").value;
+if (edad < 18 && situacion != "Soltero") {
+    alert ("Es muy joven para NO estar soltero.");
+}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,6 +2,15 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+var situacion;
+
+edad = parseInt(document.getElementById("edad").value);
+situacion = document.getElementById("estadoCivil").value;
+
+if (edad >= 18 && situacion == "Soltero") {
+    alert ("Es soltero y no es menor.");
+}
 	
 
 
