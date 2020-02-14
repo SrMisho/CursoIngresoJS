7e566133dfdbe2c4965e8adbda4aacd04995019c
SrMisho <60608645+SrMisho@users.noreply.github.com>
2020-02-14 09:24:33 -0300

    Crasheo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numero;
+	var maximo = 10;
+	var minimo = 1;
+
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	if (numero >= 9) {
+		alert ("Excelente.");
+	} else if (numero >= 4) {
+		alert ("Aprobo.");
+	} else {
+		alert ("Vamos, la proxima se puede.")
+	}
 
 }//FIN DE LA FUNCIÓN
