1e2d5523735e0079613f782ed7e7e6a7c4d81d77
SrMisho <60608645+SrMisho@users.noreply.github.com>
2020-02-17 11:09:41 -0300

    Clase 5

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave;
 
+clave = prompt("ingrese el número clave.");
+
+while (clave != "utn750" ) {
+    clave = prompt("Clave incorrecta. Ingrese la clave de nuevo.");
+    
+}
+
+alert("¡Clave correcta!");
 
 }//FIN DE LA FUNCIÓN
