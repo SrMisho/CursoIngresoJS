024b18149a87b30dafc0f28232a86abbe5829084
SrMisho <60608645+SrMisho@users.noreply.github.com>
2020-02-28 12:23:12 -0300

    Clase 7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+var nota;
+var sexo;
+var acunotas = 0;
+var promedio;
+var alumnos = 0;
 
+for (var k = 0; k < 5; k++) {
+   nota = parseInt(prompt("Ingrese su nota."));
+   while (nota < 0 || nota > 10 || isNaN(nota)) {
+    nota = parseInt(prompt("Esa nota no es valida. Ingrese su nota."));
+   }
+    sexo = prompt("Ingrese su sexo (m o f)").toLowerCase();
+    while (sexo != 'm' && sexo != 'f') {
+        sexo = prompt("Sexo invalido. Ingrese su sexo (m o f)").toLowerCase();
+    }
+acunotas = acunotas + nota;
+
+}
 }
